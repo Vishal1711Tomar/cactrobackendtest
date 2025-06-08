c645c1b2841d0668e896715b8ba8531b8e61f483
VISHAL TOMAR <tomarvishal2018@gmail.com>
2025-06-08 11:45:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+
+
+const chalk = require("chalk");
+
+console.log(chalk.green.bold("\nVishal Tomar"));
+console.log(chalk.white("Game & Frontend Developer\n"));
+console.log(chalk.cyan("📧 vishaltomar2k23@gmail.com"));
+console.log(chalk.cyan("🔗 LinkedIn:  https://www.linkedin.com/in/vishal-tomar-90ba2a1a8"));
+console.log(chalk.cyan("💻 GitHub:  https://github.com/Vishal1711Tomar"));
+console.log(chalk.cyan("🌐 Portfolio: https://vishal1711tomar.github.io/Vishal-Tomar-Portfolio\n"));
+
+console.log(chalk.yellow.bold("Experience:"));
+console.log("Argusoft India Ltd (Jul 2023 – Dec 2024)");
+console.log("- Angular, .NET, Unity, SQL based development");
+console.log("- Created hyper-casual games, FPS, AR apps, voice-based games\n");
+
+console.log(chalk.yellow.bold("Education:"));
+console.log("B.Tech in Information Technology – COER\n");
+
+console.log(chalk.yellow.bold("Skills:"));
+console.log("- Angular, React, Unity, .NET, SQL Server");
+console.log("- Game Development, AR/VR, API Integration\n");
+
+console.log(chalk.yellow.bold("Certifications:"));
+console.log("- Unity Certified Developer");
+console.log("- Angular & SQL Certifications\n");
+
+console.log(chalk.gray("Thank you for viewing my resume on the command line! 🚀\n"));
